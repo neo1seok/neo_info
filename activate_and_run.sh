c0236c7396451db3afa097e696e86b86a97d2922
WONSEOK SHIN <neo1seok@gmail.com>
2018-05-30 16:52:39 +0900

    Create activate_and_run.sh

---
+++ activate_and_run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo $PWD
+echo $PWD/vr_product/bin/activate
+source ./vr_product/bin/activate
+
+which python
+
+#nohup python -m product_machine -option run_server -config config.json &
