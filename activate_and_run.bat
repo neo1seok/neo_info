374610a47ccb8421cc4ef922e343fc198a3ec9a8
WONSEOK SHIN <neo1seok@gmail.com>
2018-05-30 16:54:06 +0900

    Update and rename install_product_machine.bat to activate_and_run.bat
    change file name

---
+++ activate_and_run.bat
@@ -0,0 +1,4 @@
+call vr_product\Scripts\activate.bat
+where python
+rem python -m pip install git+https://github.com/neo1seok/product_machine.git@master
+pause
--- install_product_machine.bat
+++
@@ -1,3 +0,0 @@
-call activate.bat
-python -m pip install git+https://github.com/neo1seok/product_machine.git@master
-pause
